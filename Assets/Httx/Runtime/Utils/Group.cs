874d051f95544513f072f721c93ed16ea3f3bfdc
Aleksandr Ovchinnikov <silago.nevermind@gmail.com>
2022-06-16 12:37:32 +0300

    add ab grouping extension

---
+++ Assets/Httx/Runtime/Utils/Group.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Httx.Requests;
+
+namespace Httx.Utils {
+  public class Group : BaseRequest {
+    private readonly string group;
+
+    public Group(IRequest next, string abGroup = null) : base(next) {
+      group = abGroup;
+    }
+
+    public override IEnumerable<KeyValuePair<string, object>> Headers =>
+        string.IsNullOrEmpty(group)
+            ? new Dictionary<string, object>()
+            : new Dictionary<string, object> { ["X-Accept-Group"] = group };
+  }
+
+  public static class GroupFluentExtensions {
+    public static IRequest Group(this IRequest request, string abGroup = null) => new Group(request, abGroup);
+  }
+}
